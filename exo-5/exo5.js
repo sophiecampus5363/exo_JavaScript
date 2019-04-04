2d4a5430b7b4126fd2ca886c2b061d9db3933952
Sophie <sophie.jacquot@le-campus_numerique.fr>
2019-04-04 13:29:57 +0200

    exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,27 @@
 // you can write js here
 
 console.log('exo-5');
+
+var input = 'Turpentine and turtles';
+var vowels = ['a','e','i','o', 'u', 'y'];
+var resultArray = [];
+
+for(let i = 0; i < input.length; i++){
+    for (let j = 0; j < vowels.length; j++){
+        if (input[i] === vowels[j]){
+            resultArray.push(input[i])
+        }
+    }
+    console.log(input[i]);
+
+    console.log(resultArray);
+
+    console.log(resultArray.join().toUpperCase());
+
+   
+
+   //console.log(vowels.indexOf(input[i]));
+
+
+}
+
